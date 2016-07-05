0ad5d90ca6b4ed74e137a90b7f8a2e8d0e18f375
edewang <edewang@gmail.com>
2016-07-05 16:02:49 -0700

    Add Biospace_Scraper Files

---
+++ BioSpace_Scraper.py
@@ -0,0 +1,42 @@
+
+# coding: utf-8
+
+# In[2]:
+
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+
+
+# In[ ]:
+
+url = "http://www.biospace.com/jobs/job-search-query/?PositionTypeList=FULL+TIME&Location=CA&Radius=50&State=California&Country=United+States&PageSize=10&PageIndex=1"
+
+def job_grabber(url, start=1):
+    soup = BeautifulSoup(urllib.request.urlopen(url), 'html.parser')
+    jobs = soup.find_all(id="spnDatePosted")
+    date = jobs[0].get_text(strip=True)
+    done = False
+    for job in jobs:
+        if job.get_text(strip=True) == date:
+            job_title = job.find_next_sibling()
+            job_employer = job_title.find_next_sibling()
+            job_location = job_employer.find_next_sibling()
+            job_desc = job_location.find_next_sibling().find_next_sibling()
+            print(str(start) + ". " + str(job_title.a))
+            print(job_employer.get_text(strip=True) + "\t" + re.sub('\s{2,}', '', job_location.get_text(strip=True)))
+            print(re.sub('\n\n', '\n', job_desc.get_text(strip=True)) + "\n")
+            start += 1
+        else:
+            done = True
+            break
+    if not done:
+        job_grabber(url[:-1] + str(int(url[-1])+1), start)
+
+job_grabber(url)
+
+
+# In[ ]:
+
+
+
